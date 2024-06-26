ce8b357be53c2b25a981f38ebad529642712f887
jaljmay187 <144557543+jaljmay187@users.noreply.github.com>
2024-06-26 19:48:52 +0300

    Update herramientas-gemini.js

--- plugins/herramientas-gemini.js
+++ plugins/herramientas-gemini.js
@@ -1,20 +1,22 @@
-import fetch from 'node-fetch'
-var handler = async (m, { text,  usedPrefix, command }) => {
-if (!text) throw `*${lenguajeGB['smsAvisoMG']()}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉𝘼 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉 𝙊 𝙐𝙉𝘼 𝙊𝙍𝘿𝙀𝙉 𝙋𝘼𝙍𝘼 𝙐𝙎𝘼𝙍 𝙇𝘼 𝙁𝙐𝙉𝘾𝙄𝙊𝙉 𝘿𝙀𝙇 𝘽𝘼𝙍𝘿\n\n❏ 𝙀𝙅𝙀𝙈𝙋𝙇𝙊 𝘿𝙀 𝙋𝙀𝙏𝙄𝘾𝙄𝙊𝙉𝙀𝙎 𝙔 𝙊𝙍𝘿𝙀𝙉𝙀𝙎\n❏ ${usedPrefix + command} Recomienda un top 10 de películas de acción\n❏ ${usedPrefix + command} Codigo en JS para un juego de cartas`
-try {
-conn.sendPresenceUpdate('composing', m.chat);
-var apii = await fetch(`https://aemt.me/gemini?text=${text}`)
-var res = await apii.json()
-await m.reply(res.result)
-} catch (e) {
-await conn.reply(m.chat, `${lenguajeGB['smsMalError3']()}#report ${lenguajeGB['smsMensError2']()} ${usedPrefix + command}\n\n${wm}`, fkontak, m)
-console.log(`❗❗ ${lenguajeGB['smsMensError2']()} ${usedPrefix + command} ❗❗`)
-console.log(e)
-}}
-handler.command = ['بارد', 'gemini']
-handler.help = ['bard', 'gemini']
-handler.tags = ['herramientas']
+import fetch from 'node-fetch';
 
-handler.premium = false
+var handler = async (m, { text, usedPrefix, command }) => {
+    if (!text) throw `*يرجى وضع نص او صورة للرد عليه.*\n\n*❏ لمزيد من الأمثلة والأوامر يرجى استخدام*\n*${usedPrefix + command} أو ${usedPrefix + command} أو ${usedPrefix + command}*`;
+    try {
+        conn.sendPresenceUpdate('composing', m.chat);
+        var apiResponse = await fetch(`https://aemt.me/gemini?text=${encodeURIComponent(text)}`);
+        var res = await apiResponse.json();
+        await m.reply(res.result);
+    } catch (error) {
+        await conn.reply(m.chat, '*حدث خطأ أثناء المعالجة.*\n\n*يرجى التبليغ عن هذا الخطأ باستخدام*\n*#report ' + usedPrefix + command + '*\n\n' + wm, fkontak, m);
+        console.log(`❗❗ حدث خطأ أثناء تنفيذ ${usedPrefix + command} ❗❗`);
+        console.error(error);
+    }
+};
 
-export default handler
+handler.command = ['جيمي', 'bard'];
+handler.help = ['تحليل', 'bard'];
+handler.tags = ['أدوات'];
+handler.premium = false;
+
+export default handler;
